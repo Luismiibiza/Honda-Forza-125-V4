cafa05e33c2685f27a5d77b96042fe60867d025c
Luismiibiza <ibiserrano@hotmail.com>
2026-08-15 17:51:37 +0200

    Refactor service worker for caching strategy
    Updated service worker to use new cache name and added activate event to manage old caches.

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
+const CACHE_NAME = "forza-v53";
+
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icono-512.png"
+];
+
 self.addEventListener("install", event => {
 
     event.waitUntil(
 
-        caches.open("forza-v4").then(cache => {
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
 
-            return cache.addAll([
-                "./",
-                "index_v4_apc.html",
-                "manifest.json",
-                "icon-512.png"
-            ]);
+    );
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+
+            );
 
         })
 
@@ -22,7 +45,11 @@ self.addEventListener("fetch", event => {
     event.respondWith(
 
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then(response => {
+
+                return response || fetch(event.request);
+
+            })
 
     );
 
